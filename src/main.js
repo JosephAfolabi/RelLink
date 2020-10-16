a398a3f0c542386f7ac63044149730911a406e8c
BerylBucket <BerylBucket@users.noreply.github.com>
2020-10-16 23:06:00 +0000

    Prettified Code!

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from "vue";
 import Home from "./Home";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFacebookSquare, faTwitter, faPinterest, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebookSquare,
+  faTwitter,
+  faPinterest,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faFacebookSquare, faTwitter, faPinterest, faInstagram)
+library.add(faFacebookSquare, faTwitter, faPinterest, faInstagram);
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 

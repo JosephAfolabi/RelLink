fad9dfe2cf9dcc77ecaa520ac2396a5541f11294
BerylBucket <71046403+BerylBucket@users.noreply.github.com>
2020-10-16 16:09:09 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
 import Vue from "vue";
 import Home from "./Home";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faFacebookSquare,
-  faTwitter,
-  faPinterest,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faFacebookSquare, faTwitter, faPinterest, faInstagram);
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
